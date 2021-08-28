976c3e88374a8bf84cb3cbf16e5866499e55885c
mufasa101 <joseph.omenda007@gmail.com>
2021-08-28 14:02:13 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -33,25 +33,5 @@ import axios from "axios";
 
 
 
-//   try {
-//     const {
-//       data: { data },
-//     } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
-//       params: {
-//         bl_latitude: sw.lat,
-//         bl_longitude: sw.lng,
-//         tr_longitude: ne.lng,
-//         tr_latitude: ne.lat,
-//       },
-//       headers: {
-//         "x-rapidapi-key": '6b48939581msh795bcbb3490f2bep1b3b1cjsn8e2d57a03760',
-//         "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-//       },
-//     });
-//     return data;
-// } catch (error) {
-//   console.log(error);
-// }
-}
 
 
